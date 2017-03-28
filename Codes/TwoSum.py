622b803360d990f4b214efbdebdfbee097267aaa
Pod5GS <wcincredible@live.com>
2017-03-28 13:48:32 +0800

    Add Description and Example to TwoSum

--- Codes/TwoSum.py
+++ Codes/TwoSum.py
@@ -1,8 +1,19 @@
 """
+Description:
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+Solution:
 The key to the solution is to use a HashMap to map the value and the index of original array.
 Iterate the array, look back to check if current element's complement already exists in the table,
 If it exists, the result is found and return it.
 If not, insert current element into the table.
+
 Time complexity: O(N)
 Why?
 The time complexity of lookup for keys in HashTable and insert/acquire elements in HashTable are O(1).
